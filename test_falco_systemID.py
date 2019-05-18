f9c14c44ba9b31266c5f1af1fc08d15c81c47b4d
groot <hesun@princeton.edu>
2019-05-18 17:26:48 -0400

    modified the falco_systemID codes for testing

--- test_falco_systemID.py
+++ test_falco_systemID.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 	camera = detector.CCD(flux=2e9, readout_std=12, readout=True, photon=True, exp_time=exp_time)
 
 	# define the system identifier for adaptive control
-	n_batch = 5
+	n_batch = 20
 	# vl = sysid.linear_vl(params_values, img_number//2)
 
 
@@ -112,163 +112,163 @@ if __name__ == "__main__":
 	# Ef_est_set = []
 	# Ef_vector_set = []
 	# P_est_set = []
-	# contrast_set = []
-	# n_trials = 1
-	# for trial in range(n_trials):
-	# start wavefront control
-	u1 = np.zeros((34, 34))
-	u2 = np.zeros((34, 34))
-	u10 = u1
-	u20 = u2
-	contrast = np.zeros((Nitr, ))
-	E_true = []
-	u_p_nominal_set = []
-	u_p_optimal_set = []
-	for k in range(Nitr):
-		# collect the unprobed image
-		Ef = model.Propagate(u1, u2)
-		# Ef = sp.ndimage.shift(Ef.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef.imag, shift=[0.5, 0.5, 0])
-		Ef_vector = Ef[dh_ind1, dh_ind2, :]
-		If_vector = np.abs(Ef_vector)**2
-		contrast[k] = np.mean(If_vector)
-		If = camera.Add_noise(If_vector)
-		if k == 0:
-			Ef0 = np.array(Ef)
-		print('The contrast at step #{} is {}'.format(k, contrast[k]))
-		E_true.append(Ef_vector)
-		
-		# collect probe images
-		# camera.set_exposure(np.max([3.7e-10/contrast[k], 1e-2]))
-		exp_time = camera.exp_time
-		print('The exposure time at step #{} is {}'.format(k, exp_time))
+	contrast_set = []
+	n_trials = 20
+	for trial in range(n_trials):
+		# start wavefront control
+		u1 = np.zeros((34, 34))
+		u2 = np.zeros((34, 34))
+		u10 = u1
+		u20 = u2
+		contrast = np.zeros((Nitr, ))
+		E_true = []
+		u_p_nominal_set = []
+		u_p_optimal_set = []
+		for k in range(Nitr):
+			# collect the unprobed image
+			Ef = model.Propagate(u1, u2)
+			Ef = sp.ndimage.shift(Ef.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef.imag, shift=[0.5, 0.5, 0])
+			Ef_vector = Ef[dh_ind1, dh_ind2, :]
+			If_vector = np.abs(Ef_vector)**2
+			contrast[k] = np.mean(If_vector)
+			If = camera.Add_noise(If_vector)
+			if k == 0:
+				Ef0 = np.array(Ef)
+			print('The contrast at step #{} is {}'.format(k, contrast[k]))
+			E_true.append(Ef_vector)
+			
+			# collect probe images
+			# camera.set_exposure(np.max([3.7e-10/contrast[k], 1e-2]))
+			exp_time = camera.exp_time
+			print('The exposure time at step #{} is {}'.format(k, exp_time))
 
-		R_coef = [params_values['R0']/exp_time**2, params_values['R1']/exp_time+4*params_values['Q0'], 4*params_values['Q1']]
-		u_p = sensor0.Probe_command(contrast[k], k, index=1, R_coef=R_coef)
-		
-		# # u_p_nominal_set.append(np.array(u_p))
+			R_coef = [params_values['R0']/exp_time**2, params_values['R1']/exp_time+4*params_values['Q0'], 4*params_values['Q1']]
+			u_p = sensor0.Probe_command(contrast[k], k, index=1, R_coef=R_coef)
+			
+			# # u_p_nominal_set.append(np.array(u_p))
 
-		# u_p_values = u_p[model.DMind1, model.DMind2, :].T
+			# u_p_values = u_p[model.DMind1, model.DMind2, :].T
 
-		# contrast_p = np.sqrt(params_values['R0']/exp_time**2/(4*params_values['Q1']) + \
-		# 			(params_values['R1']/exp_time + 4*params_values['Q0'])*contrast[k]/(4*params_values['Q1']))
-		# print('designed probe contrast is {}.'.format(contrast_p))
+			# contrast_p = np.sqrt(params_values['R0']/exp_time**2/(4*params_values['Q1']) + \
+			# 			(params_values['R1']/exp_time + 4*params_values['Q0'])*contrast[k]/(4*params_values['Q1']))
+			# print('designed probe contrast is {}.'.format(contrast_p))
 
-		# u_p = np.zeros((model.Nact, model.Nact, img_number//2))
-		# # u_p_values = 1e1 * np.sqrt(contrast_p) * np.random.rand(img_number//2, n_act)
+			# u_p = np.zeros((model.Nact, model.Nact, img_number//2))
+			# # u_p_values = 1e1 * np.sqrt(contrast_p) * np.random.rand(img_number//2, n_act)
 
-		# u_p_values = sensor.Probe_command(u_p_values, exp_time, contrast[k], rate=5e-4, beta=0., gamma=1., Nitr=2000, print_flag=True)
-		# u_p[model.DMind1, model.DMind2, :] = u_p_values.T
+			# u_p_values = sensor.Probe_command(u_p_values, exp_time, contrast[k], rate=5e-4, beta=0., gamma=1., Nitr=2000, print_flag=True)
+			# u_p[model.DMind1, model.DMind2, :] = u_p_values.T
 
-		# # u_p_optimal_set.append(np.array(u_p))
-		
-		if contrast[k] > 0:#contrast_p:
-			If_p = np.empty((len(dh_ind1), len(wavelength), img_number), dtype=float)
-			Ef_p_set = np.empty((len(dh_ind1), len(wavelength), img_number), dtype=complex)
-			for i in range(u_p.shape[2]):
-				# images with positive probes
-				Ef_p = model.Propagate(u1+u_p[:, :, i], u2)
-				# Ef_p = sp.ndimage.shift(Ef_p.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef_p.imag, shift=[0.5, 0.5, 0])
-				Ef_p_vector = Ef_p[dh_ind1, dh_ind2, :]
-				If_p_vector = np.abs(Ef_p_vector)**2
-				# If_p[:, :, 2*i] = If_p_vector
-				If_p[:, :, 2*i] = camera.Add_noise(If_p_vector)
-				Ef_p_set[:, :, 2*i] = Ef_p_vector
-				print('The contrast of the No.{} postive image is {}'.format(i, np.mean(If_p_vector)))
-				# images with negative probes
-				Ef_p = model.Propagate(u1-u_p[:, :, i], u2)
-				# Ef_p = sp.ndimage.shift(Ef_p.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef_p.imag, shift=[0.5, 0.5, 0])
-				Ef_p_vector = Ef_p[dh_ind1, dh_ind2, :]
-				If_p_vector = np.abs(Ef_p_vector)**2
-				# If_p[:, :, 2*i+1] = If_p_vector
-				If_p[:, :, 2*i+1] = camera.Add_noise(If_p_vector)
-				Ef_p_set[:, :, 2*i+1] = Ef_p_vector
-				print('The contrast of the No.{} negative image is {}'.format(i, np.mean(If_p_vector)))
-				# print('The contrast of the No.{} difference image is {}'.format(i, np.mean(np.abs(If_p[:, :, 2*i] - If_p[:, :, 2*i+1]))))
-		else:
-			If_p = np.empty((len(dh_ind1), len(wavelength), img_number//2), dtype=float)
-			Ef_p_set = np.empty((len(dh_ind1), len(wavelength), img_number//2), dtype=complex)
-			for i in range(u_p.shape[2]):
-				# images with positive probes
-				Ef_p = model.Propagate(u1+u_p[:, :, i], u2)
-				# Ef_p = sp.ndimage.shift(Ef_p.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef_p.imag, shift=[0.5, 0.5, 0])
-				Ef_p_vector = Ef_p[dh_ind1, dh_ind2, :]
-				If_p_vector = np.abs(Ef_p_vector)**2
-				# If_p[:, :, 2*i] = If_p_vector
-				If_p[:, :, i] = camera.Add_noise(If_p_vector)
-				Ef_p_set[:, :, i] = Ef_p_vector
-				print('The contrast of the No.{} postive image is {}'.format(i, np.mean(If_p_vector)))
-
-		# Ef_est = Ef_vector
-		if k <= 100:
-			u_p_vector = u_p[model.DMind1, model.DMind2, :]
-			Ef_est, P_est = BPE_estimator.Estimate(If_p, u_p_vector, np.zeros(u_p_vector.shape), exp_time)
-		else:
-			if k == 1:
-				P_est = 4 * P_est
-				Iinco_est = np.zeros(Ef_est.shape)
+			# # u_p_optimal_set.append(np.array(u_p))
+			
 			if contrast[k] > 0:#contrast_p:
-				u_p_vector = u_p[model.DMind1, model.DMind2, :]
-				Ef_est, P_est = KF_estimator.Estimate(If_p, u_p_vector, np.zeros(u_p_vector.shape), Ef_est, P_est, 
-													command[0:n_act], command[n_act::], exp_time)
+				If_p = np.empty((len(dh_ind1), len(wavelength), img_number), dtype=float)
+				Ef_p_set = np.empty((len(dh_ind1), len(wavelength), img_number), dtype=complex)
+				for i in range(u_p.shape[2]):
+					# images with positive probes
+					Ef_p = model.Propagate(u1+u_p[:, :, i], u2)
+					Ef_p = sp.ndimage.shift(Ef_p.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef_p.imag, shift=[0.5, 0.5, 0])
+					Ef_p_vector = Ef_p[dh_ind1, dh_ind2, :]
+					If_p_vector = np.abs(Ef_p_vector)**2
+					# If_p[:, :, 2*i] = If_p_vector
+					If_p[:, :, 2*i] = camera.Add_noise(If_p_vector)
+					Ef_p_set[:, :, 2*i] = Ef_p_vector
+					print('The contrast of the No.{} postive image is {}'.format(i, np.mean(If_p_vector)))
+					# images with negative probes
+					Ef_p = model.Propagate(u1-u_p[:, :, i], u2)
+					Ef_p = sp.ndimage.shift(Ef_p.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef_p.imag, shift=[0.5, 0.5, 0])
+					Ef_p_vector = Ef_p[dh_ind1, dh_ind2, :]
+					If_p_vector = np.abs(Ef_p_vector)**2
+					# If_p[:, :, 2*i+1] = If_p_vector
+					If_p[:, :, 2*i+1] = camera.Add_noise(If_p_vector)
+					Ef_p_set[:, :, 2*i+1] = Ef_p_vector
+					print('The contrast of the No.{} negative image is {}'.format(i, np.mean(If_p_vector)))
+					# print('The contrast of the No.{} difference image is {}'.format(i, np.mean(np.abs(If_p[:, :, 2*i] - If_p[:, :, 2*i+1]))))
 			else:
-				# u_p_vector = np.zeros((n_act, img_number))
-				# u_p_vector[:, 0::2] = u_p[model.DMind1, model.DMind2, :]
-				# u_p_vector[:, 1::2] = -u_p[model.DMind1, model.DMind2, :]
+				If_p = np.empty((len(dh_ind1), len(wavelength), img_number//2), dtype=float)
+				Ef_p_set = np.empty((len(dh_ind1), len(wavelength), img_number//2), dtype=complex)
+				for i in range(u_p.shape[2]):
+					# images with positive probes
+					Ef_p = model.Propagate(u1+u_p[:, :, i], u2)
+					# Ef_p = sp.ndimage.shift(Ef_p.real, shift=[0.5, 0.5, 0]) + 1j * sp.ndimage.shift(Ef_p.imag, shift=[0.5, 0.5, 0])
+					Ef_p_vector = Ef_p[dh_ind1, dh_ind2, :]
+					If_p_vector = np.abs(Ef_p_vector)**2
+					# If_p[:, :, 2*i] = If_p_vector
+					If_p[:, :, i] = camera.Add_noise(If_p_vector)
+					Ef_p_set[:, :, i] = Ef_p_vector
+					print('The contrast of the No.{} postive image is {}'.format(i, np.mean(If_p_vector)))
+
+			# Ef_est = Ef_vector
+			if k <= 100:
 				u_p_vector = u_p[model.DMind1, model.DMind2, :]
-				Ef_est, Iinco_est, P_est = EKF_estimator.Estimate(If_p, u_p_vector, np.zeros(u_p_vector.shape), Ef_est, Iinco_est, P_est, 
-													command[0:n_act], command[n_act::], exp_time)
-		# compute control command
-		command = EFC(Ef_est, G, weight, alpha)
-		u1[model.DMind1, model.DMind2] += command[:int(len(command)/2):]
-		u2[model.DMind1, model.DMind2] += command[int(len(command)/2)::]
+				Ef_est, P_est = BPE_estimator.Estimate(If_p, u_p_vector, np.zeros(u_p_vector.shape), exp_time)
+			else:
+				if k == 1:
+					P_est = 4 * P_est
+					Iinco_est = np.zeros(Ef_est.shape)
+				if contrast[k] > 0:#contrast_p:
+					u_p_vector = u_p[model.DMind1, model.DMind2, :]
+					Ef_est, P_est = KF_estimator.Estimate(If_p, u_p_vector, np.zeros(u_p_vector.shape), Ef_est, P_est, 
+														command[0:n_act], command[n_act::], exp_time)
+				else:
+					# u_p_vector = np.zeros((n_act, img_number))
+					# u_p_vector[:, 0::2] = u_p[model.DMind1, model.DMind2, :]
+					# u_p_vector[:, 1::2] = -u_p[model.DMind1, model.DMind2, :]
+					u_p_vector = u_p[model.DMind1, model.DMind2, :]
+					Ef_est, Iinco_est, P_est = EKF_estimator.Estimate(If_p, u_p_vector, np.zeros(u_p_vector.shape), Ef_est, Iinco_est, P_est, 
+														command[0:n_act], command[n_act::], exp_time)
+			# compute control command
+			command = EFC(Ef_est, G, weight, alpha)
+			u1[model.DMind1, model.DMind2] += command[:int(len(command)/2):]
+			u2[model.DMind1, model.DMind2] += command[int(len(command)/2)::]
 
-		if save_data_flag:
-			data_train_all['u1'][:, k] = command[0:n_act]
-			data_train_all['u2'][:, k]= command[n_act::]
-			for k_image in range(img_number):
-				data_train_all['u1p'][:, k_image, k] = ((-1)**(k_image%2)) * u_p[model.DMind1, model.DMind2, k_image//2]
-				data_train_all['u2p'][:, k_image, k] = np.zeros((n_act, ))
-			data_train_all['I'][:, :, k] = np.concatenate([np.squeeze(If).reshape((-1, 1)), np.squeeze(If_p)], 1)
-			data_train_all['time'][k] = camera.exp_time
-			data_train_all['contrast'] = contrast
-				
-			# Ef_est_set.append(Ef_est)
-			# Ef_vector_set.append(Ef_vector)
-			# P_est_set.append(P_est)
+			if save_data_flag:
+				data_train_all['u1'][:, k] = command[0:n_act]
+				data_train_all['u2'][:, k]= command[n_act::]
+				for k_image in range(img_number):
+					data_train_all['u1p'][:, k_image, k] = ((-1)**(k_image%2)) * u_p[model.DMind1, model.DMind2, k_image//2]
+					data_train_all['u2p'][:, k_image, k] = np.zeros((n_act, ))
+				data_train_all['I'][:, :, k] = np.concatenate([np.squeeze(If).reshape((-1, 1)), np.squeeze(If_p)], 1)
+				data_train_all['time'][k] = camera.exp_time
+				data_train_all['contrast'] = contrast
+					
+				# Ef_est_set.append(Ef_est)
+				# Ef_vector_set.append(Ef_vector)
+				# P_est_set.append(P_est)
 
-			# if (k+1) % n_batch == 0:
-			# 	data_train = {}
-			# 	data_train['u1'] = data_train_all['u1'][:, k+1-n_batch:k+1]
-			# 	data_train['u2'] = data_train_all['u2'][:, k+1-n_batch:k+1]
-			# 	data_train['u1p'] = data_train_all['u1p'][:, :, k+1-n_batch:k+1]
-			# 	data_train['u2p'] = data_train_all['u2p'][:, :, k+1-n_batch:k+1]
-			# 	data_train['I'] = data_train_all['I'][:, :, k+1-n_batch:k+1]
-				
-			# 	sio.savemat(path2data+'data_train.mat', {'u1': data_train_all['u1'][:, k+1-n_batch:k+1],
-			# 							'u2': data_train_all['u2'][:, k+1-n_batch:k+1],
-			# 							'u1p': data_train_all['u1p'][:, :, k+1-n_batch:k+1],
-			# 							'u2p': data_train_all['u2p'][:, :, k+1-n_batch:k+1],
-			# 							'I': data_train_all['I'][:, :, k+1-n_batch:k+1]})
-			# 	if (k+1)==n_batch:
-			# 		IDnet = falco.vl_net(Q0=1e-14, Q1=0.05, R0=3.6e-17, R1=5e-10, path2data=path2data)
-			# 		falco.linear_vl(IDnet, lr=1e-8, lr2=1e-5, epoch=10, print_flag=True)
-			# 	else:
-			# 		falco.linear_vl(IDnet, lr=1e-8, lr2=1e-5, epoch=10, print_flag=True)
+				if (k+1) % n_batch == 0:
+					data_train = {}
+					data_train['u1'] = data_train_all['u1'][:, k+1-n_batch:k+1]
+					data_train['u2'] = data_train_all['u2'][:, k+1-n_batch:k+1]
+					data_train['u1p'] = data_train_all['u1p'][:, :, k+1-n_batch:k+1]
+					data_train['u2p'] = data_train_all['u2p'][:, :, k+1-n_batch:k+1]
+					data_train['I'] = data_train_all['I'][:, :, k+1-n_batch:k+1]
+					
+					sio.savemat(path2data+'data_train.mat', {'u1': data_train_all['u1'][:, k+1-n_batch:k+1],
+											'u2': data_train_all['u2'][:, k+1-n_batch:k+1],
+											'u1p': data_train_all['u1p'][:, :, k+1-n_batch:k+1],
+											'u2p': data_train_all['u2p'][:, :, k+1-n_batch:k+1],
+											'I': data_train_all['I'][:, :, k+1-n_batch:k+1]})
+					if (k+1)==n_batch and trial==0:
+						IDnet = falco.vl_net(Q0=1e-14, Q1=0.05, R0=3.6e-17, R1=5e-10, path2data=path2data)
+						falco.linear_vl(IDnet, lr=1e-7, lr2=1e-3, epoch=10, print_flag=True)
+					else:
+						falco.linear_vl(IDnet, lr=1e-7, lr2=1e-3, epoch=10, print_flag=True)
 
-			# 	Jacobian = sio.loadmat((path2data+'jacStructLearned.mat'))
-			# 	G1 = np.expand_dims(Jacobian['G1'], -1)
-			# 	G2 = np.expand_dims(Jacobian['G2'], -1)
-			# 	noise_coef = np.squeeze(Jacobian['noise_coef'])
+					Jacobian = sio.loadmat((path2data+'jacStructLearned.mat'))
+					G1 = np.expand_dims(Jacobian['G1'], -1)
+					G2 = np.expand_dims(Jacobian['G2'], -1)
+					noise_coef = np.squeeze(Jacobian['noise_coef'])
 
-			# 	params_values['G1'] = G1
-			# 	params_values['G2'] = G2
-			# 	params_values['Q0'] = noise_coef[0]
-			# 	params_values['Q1'] = noise_coef[1]
-			# 	params_values['R0'] = noise_coef[2]
-			# 	params_values['R1'] = noise_coef[3]
+					params_values['G1'] = G1
+					params_values['G2'] = G2
+					params_values['Q0'] = noise_coef[0]
+					params_values['Q1'] = noise_coef[1]
+					params_values['R0'] = noise_coef[2]
+					params_values['R1'] = noise_coef[3]
 
-			# 	G = np.concatenate((G1, G2), axis=1)
+					G = np.concatenate((G1, G2), axis=1)
 
-		# contrast_set.append(contrast)
+		contrast_set.append(contrast)
 
 			
